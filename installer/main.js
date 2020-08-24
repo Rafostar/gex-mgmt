51a83ad4ece9ce66aff01d788b8065664c3f2ce9
Rafostar <40623528+Rafostar@users.noreply.github.com>
2020-08-24 20:52:57 +0200

    Add install directories page

--- installer/main.js
+++ installer/main.js
@@ -3,6 +3,10 @@ imports.gi.versions.Gtk = '3.0';
 const { Gio, GLib, Gtk, Soup } = imports.gi;
 
 const APP_NAME = 'Gex Installer';
+const DATA_DIR = GLib.get_user_data_dir();
+const INSTALL_DIR = DATA_DIR + '/gex';
+const PREFIX_DIR = DATA_DIR.slice(0, DATA_DIR.lastIndexOf('/'));
+const BIN_PATH = PREFIX_DIR + '/bin/gex';
 
 class Downloader
 {
@@ -119,16 +123,15 @@ class Installer
 
         let index = 1;
         while(true) {
-            let getPage = this[`_getPage${index}`];
-            if(typeof getPage !== 'function')
+            if(typeof this[`_getPage${index}`] !== 'function')
                 break;
 
-            let [page, title, type] = getPage();
+            let [page, title, type] = this[`_getPage${index}`]();
 
             this.assistant.append_page(page);
             this.assistant.set_page_title(page, title);
             this.assistant.set_page_type(page, Gtk.AssistantPageType[type]);
-            this.assistant.set_page_complete(page, true);
+            this.assistant.set_page_complete(page, (type !== 'PROGRESS'));
 
             index++;
         }
@@ -136,20 +139,79 @@ class Installer
         Gtk.main();
     }
 
+    getLabel(text, size)
+    {
+        size = size || 12;
+
+        return new Gtk.Label({
+            label: `<span font="${size}"><b>` + text + '</b></span>',
+            use_markup: true
+        });
+    }
+
+    addConfigToGrid(grid, text, value)
+    {
+        let label = new Gtk.Label({ label: text });
+        let entry = new Gtk.Entry({
+            text: value,
+            width_chars: 40,
+            sensitive: false
+        });
+
+        grid.attach(label, 0, this.configRow, 1, 1);
+        grid.attach(entry, 1, this.configRow, 1, 1);
+
+        this.configRow++;
+    }
+
     _getPage1()
     {
-        let label = new Gtk.Label({
-            label: `Welcome to the ${APP_NAME}`
-        });
+        let label = this.getLabel(`Welcome to the ${APP_NAME}`, 14);
 
         return [label, 'Introduction', 'INTRO'];
     }
 
     _getPage2()
     {
-        let label = new Gtk.Label({
-            label: 'Gex was successfully installed'
+        let grid = new Gtk.Grid({
+            halign:Gtk.Align.CENTER,
+            valign:Gtk.Align.CENTER,
+            row_spacing: 8,
+            column_spacing: 8
         });
+
+        let label = this.getLabel(`Gex will be installed to these directories`);
+        label.margin_bottom = 8;
+        grid.attach(label, 0, 0, 2, 1);
+
+        this.configRow = 1;
+        this.addConfigToGrid(grid, 'Prefix Path', PREFIX_DIR);
+        this.addConfigToGrid(grid, 'Install Path', INSTALL_DIR);
+        this.addConfigToGrid(grid, 'Binary Path', BIN_PATH);
+
+        return [grid, 'Info', 'CONTENT'];
+    }
+
+    _getPage3()
+    {
+        let grid = new Gtk.Grid({
+            halign:Gtk.Align.CENTER,
+            valign:Gtk.Align.CENTER
+        });
+        let label = this.getLabel('Installing...');
+        let progress = new Gtk.LevelBar({
+            width_request: 420
+        });
+
+        grid.attach(label, 0, 0, 1, 1);
+        grid.attach(progress, 0, 1, 1, 1);
+
+        return [grid, 'Installation', 'PROGRESS'];
+    }
+
+    _getPage4()
+    {
+        let label = this.getLabel('Gex was successfully installed');
 
         return [label, 'Finish', 'SUMMARY'];
     }
